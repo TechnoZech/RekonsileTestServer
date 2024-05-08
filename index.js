25ff4251d253654365ad2b0021ff4806e2f3cf4a
DHARMESH SHARMA <sharmadharmesh1998@gmail.com>
2024-05-09 01:01:30 +0530

    revert back the code from logginService

--- index.js
+++ index.js
@@ -1,53 +1,38 @@
 const express = require("express");
+var cors = require("cors");
 const mongoose = require("mongoose");
-const LoggingService = require("./middlewares/loggingService");
 
-
+// ! initializing packages
 const app = express();
 require("dotenv").config();
 
-// Initialize logging service
-const loggingService = new LoggingService(process.env.LOG_LEVEL);
-
-// Database connection using Mongoose
+// ! database connection using Mongoose
 mongoose
-  .connect(process.env.MONGO_URI)
-  .then(() => {
-    loggingService.log('info', "db Connected!");
-  })
-  .catch((error) => {
-    loggingService.logError(error);
-  });
+	.connect(process.env.MONGO_URI)
+	.then(() => {
+		console.log("db Connected!");
+	})
+	.catch((error) => {
+		console.log(error);
+	});
 
+// ! Cors
+app.use(cors({ origin: process.env.CORS_URI, credentials: true }));
 
 app.get("/", function (req, res) {
-  res.send("server is working");
+	res.send("server is working");
 });
 
-// Server configuration
+// ! server configuration
 app.set("view engine", "ejs");
 app.use(express.urlencoded({ extended: true }));
 
-// Middleware to log incoming requests
-app.use(loggingService.logRequest.bind(loggingService));
-
-// Requiring routes
-const auth = require("./routes/auth");
+// ! requiring routes
+const auth = require("./Routes/auth");
 app.use(auth);
 
-// Middleware to log response
-app.use((req, res, next) => {
-  res.on('finish', () => {
-    loggingService.logResponse(req, res);
-  });
-  next();
-});
-
-// Add listeners for uncaught exceptions and unhandled rejections
-loggingService.setupUncaughtExceptionHandler();
-loggingService.setupUnhandledRejectionHandler();
-
-// Listening to port
+// ! listening to port
+// const port = process.env.PORT;
 app.listen(process.env.PORT, () => {
-  loggingService.log('info', `server started on port ${process.env.PORT}`);
+	console.log(`server started on port ${process.env.PORT}`);
 });
